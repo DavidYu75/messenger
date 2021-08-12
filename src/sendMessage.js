0a4eb0b8569b78043c85652f363d22344c2acd9f
DavidYu75 <81201339+DavidYu75@users.noreply.github.com>
2021-08-12 00:16:03 -0400

    Update sendMessage.js

--- src/sendMessage.js
+++ src/sendMessage.js
@@ -20,38 +20,36 @@ const SendMessage = () => {
   };
 
   return (
-    <>
-      <article>
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="form-control">
-            <label htmlFor="firstName">Message: </label>
-            <input
-              type="text"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
+    <article>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="firstName">Message: </label>
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <button type="submit" onClick={handleSubmit}>
+          <img
+            src="https://img.flaticon.com/icons/png/512/60/60525.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF"
+            alt="Send"
+            height="30"
+            widht="30"
+          />
+        </button>
+      </form>
+      {chat.map((message) => {
+        return (
+          <div className="message" key={message}>
+            <h4>David: {message}</h4>
+            <button className="btn" onClick={removeMessage}>
+              Delete
+            </button>
           </div>
-          <button type="submit" onClick={handleSubmit}>
-            <img
-              src="https://img.flaticon.com/icons/png/512/60/60525.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF"
-              alt="Send"
-              height="30"
-              widht="30"
-            />
-          </button>
-        </form>
-        {chat.map((message) => {
-          return (
-            <div className="message" key={message}>
-              <h4>David: {message}</h4>
-              <button className="btn" onClick={removeMessage}>
-                Delete
-              </button>
-            </div>
-          );
-        })}
-      </article>
-    </>
+        );
+      })}
+    </article>
   );
 };
 
